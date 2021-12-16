d72efeeef234f52cce0ae28f1d2e6327852e2197
Luong Tung <luongngocsontung@gmail.com>
2021-12-16 17:45:45 +0700

    just a few change

--- src/components/Edit/Spacing/Padding.js
+++ src/components/Edit/Spacing/Padding.js
@@ -34,7 +34,6 @@ export default function Padding({ element }) {
             default:
                 break;
         }
-        
         element.updateState({ style: newStyle });
     }
 
--- src/components/SelectedDiv/SelectedDiv.js
+++ src/components/SelectedDiv/SelectedDiv.js
@@ -2,6 +2,7 @@ import "./SelectedDiv.css"
 import {DeleteOutlined} from '@ant-design/icons'
 import { createSubscription, useSubscription } from "global-state-hook";
 import getParentElement from "../../controller/getParentElement";
+import { hideSelectedDiv } from "../../controller/showSelectedDiv";
 
 const selectedDiv = createSubscription({
     visibility: "hidden",
@@ -17,6 +18,7 @@ export default function SelectedDiv() {
         const index = parent.state.childrenId.findIndex(id => id===state.id);
         parent.state.childrenId.splice(index, 1);
         parent.updateState({childrenId: parent.state.childrenId});
+        hideSelectedDiv();
     }
 
     return (
